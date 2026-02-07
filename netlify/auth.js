883caee9003d000c40412775e6c5e5fc0bf2ecfd
ranio88smb88-hub <ranio88.smb88@gmail.com>
2026-02-07 07:20:13 +0700

    Add authentication handler in auth.js

--- netlify/auth.js
+++ netlify/auth.js
@@ -1,1 +1,7 @@
-
+exports.handler = async (event, context) => {
+    // Untuk autentikasi tambahan jika diperlukan
+    return {
+        statusCode: 200,
+        body: JSON.stringify({ message: 'Auth function' })
+    };
+};
